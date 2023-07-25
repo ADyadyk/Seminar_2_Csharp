bfc74badc797bd689f60664ee25d261395af3ee2
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-07-25 20:33:18 +0300

    classwork

---
+++ Task001/Program.cs
@@ -0,0 +1,13 @@
+﻿//Разбирала преподаватель:
+
+int num = new Random().Next(1000);
+string NumStr = num.ToString();
+
+if (NumStr.Length == 3)
+{
+    Console.WriteLine($"Число {num} без второй цифры имеет вид {NumStr[0]}{NumStr[2]}");
+}
+else
+{
+    Console.WriteLine($"Число {num} не трёхзначное");
+}
---
+++ Task005/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4. Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+
+Console.Write("Введите число a=");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число b=");
+int b = int.Parse(Console.ReadLine()!);
+
+int sqra = Convert.ToInt32(Math.Pow(a, 2));
+int sqrb = Convert.ToInt32(Math.Pow(b, 2));
+
+if (a == sqrb) Console.Write($"Число a={a} является квадратом числа b={b}");
+if (b == sqra) Console.Write($"Число b={b} является квадратом числа a={a}");
+if (b != sqra && a != sqrb) Console.Write($"Число a не является квадратом числа b. И число b не является квадратом числа a.");
